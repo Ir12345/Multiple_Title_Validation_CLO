f848435a375c411f68fc23438c799a8244aa63b9
246_135 <My_email_adress@gmail.com>
2017-01-06 14:30:23 -0800

    Initial commit

---
+++ src/main/java/core/Multiple_Title_Validation_CLO.java
@@ -0,0 +1,71 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.*;
+import com.beust.jcommander.*;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+@Parameters(separators = "=")
+public class Multiple_Title_Validation_CLO {
+
+	@Parameter(names = { "-l", "--list" }, variableArity = true, description = "Your list")
+	static List<String> list = new ArrayList<String>();
+
+	@Parameter(names = "--help", help = true, hidden = true)
+	static boolean help;
+
+	public static void main(String[] args) {
+
+		JCommander clo = new JCommander(new Multiple_Title_Validation_CLO(), args);
+
+		if (help) {
+			clo.usage();
+			System.exit(0);
+		}
+
+		if (list.size() == 0) {
+			System.err.println("List is empty");
+			System.exit(1);
+		}
+			
+		WebDriver driver = new FirefoxDriver();
+
+		for (String l : list) {
+			System.out.println("");
+			System.out.println("List item = " + l);
+			
+			String text_case_id = "TC-001.0" + (list.indexOf(l) + 1);
+			
+			String param[] = l.split("\\|");
+
+			String url = param[0];
+			String title_expected = param[1];
+
+			driver.get(url);
+			driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+			String title_actual = driver.getTitle();
+			
+			if (title_expected.equals(title_actual)) {
+				System.out.println("Test Case ID: \t\t" + text_case_id);
+				System.out.println("URL: \t\t\t" + url);
+				System.out.println("Title Expected: \t" + title_expected);
+				System.out.println("Title Actual: \t\t" + title_actual);
+				System.out.println("Test Case Result: \t" + "PASSED");
+			} else {
+				System.out.println("Test Case ID: \t\t" + text_case_id);
+				System.out.println("URL: \t\t\t" + url);
+				System.out.println("Title Expected: \t" + title_expected);
+				System.out.println("Title Actual: \t\t" + title_actual);
+				System.out.println("Test Case Result: \t" + "FAILED");
+			}
+
+		}
+
+		driver.quit();
+
+	}
+
+}
